546302646652157b99e8ee22caf8939c42313002
terryc1993 <95531609+terryc1993@users.noreply.github.com>
2022-01-17 17:22:35 +0200

    Initial Upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var randomNumber1 = Math.random();
+randomNumber1 = randomNumber1 * 6;
+randomNumber1 = Math.floor(randomNumber1 + 1);
+console.log(randomNumber1);
+document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+
+var randomNumber2 = Math.random();
+randomNumber2 = randomNumber2 * 6;
+randomNumber2 = Math.floor(randomNumber2 + 1);
+console.log(randomNumber2);
+document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png")
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩Player 1 Wins";
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins🚩";
+} else {
+  document.querySelector("h1").innerHTML = "🚩Draw!🚩";
+}
